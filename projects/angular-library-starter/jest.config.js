17875a0226df27262dd3dc0ce3382aec750ba6e3
Nivek <kevin.kreuzer90@icloud.com>
2024-06-23 18:43:21 +0200

    Initial commit

---
+++ projects/angular-library-starter/jest.config.js
@@ -0,0 +1,9 @@
+const baseConfig = require('../../jest.config');
+
+module.exports = {
+  ...baseConfig,
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+  },
+  preset: 'jest-preset-angular',
+};
